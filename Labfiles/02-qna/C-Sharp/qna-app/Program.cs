fabfde57a60e1ae503f6ca7911acc5711484126b
neenoxavier <40322661+neenoxavier@users.noreply.github.com>
2024-03-06 08:29:55 -0800

    Update Program.cs

--- Labfiles/02-qna/C-Sharp/qna-app/Program.cs
+++ Labfiles/02-qna/C-Sharp/qna-app/Program.cs
@@ -1,7 +1,11 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Text;
 using Microsoft.Extensions.Configuration;
+using Azure.AI.Language.QuestionAnswering;
+using Azure;
+using System.Runtime.InteropServices;
+
 
 // Import namespaces
 
@@ -23,9 +27,25 @@ namespace qna_app
                 string deploymentName = configuration["QADeploymentName"];
 
                 // Create client using endpoint and key
-
+                // Create client using endpoint and key
+                AzureKeyCredential credentials = new AzureKeyCredential(aiSvcKey);
+                Uri endpoint = new Uri(aiSvcEndpoint);
+                QuestionAnsweringClient aiClient = new QuestionAnsweringClient(endpoint, credentials);
 
                 // Submit a question and display the answer
+                string user_question = "";
+                while(user_question.ToLower() != "quit"){
+                    Console.WriteLine("Question : ");
+                    user_question = Console.ReadLine();
+                    QuestionAnsweringProject project = new QuestionAnsweringProject(projectName,deploymentName);
+                    Response<AnswersResult> response = aiClient.GetAnswers(user_question,project);
+                    foreach(KnowledgeBaseAnswer answer in response.Value.Answers){
+                        Console.WriteLine(answer.Answer);
+                        Console.WriteLine($"Confidence: {answer.Confidence:P2}");
+                        Console.WriteLine($"Source: {answer.Source}");
+                        Console.WriteLine();
+                    }
+                }
                 
 
             }
